f3591d49a06acc45626eda2b17460d09b74835d0
Mel Fabella <mf820am@gmail.com>
2020-12-22 18:12:15 -0800

    define routes

--- src/App.js
+++ src/App.js
@@ -1,25 +1,19 @@
-import logo from './logo.svg';
-import './App.css';
-
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
-
+import { Container } from 'semantic-ui-react';
+import { Route, Switch } from 'react-router-dom';
+import Home from './components/shared/Home';
+import NoMatch from './components/shared/NoMatch';
+import Profile from './components/profile/Profile';
+import Navbar from './components/shared/Navbar';
+const App = () => (
+  <>
+    <Navbar />
+    <Container>
+      <Switch>
+        <Route exact path='/' component={Home} />
+        <Route exact path='/profile' component={Profile} />
+        <Route component={NoMatch} />
+      </Switch>
+    </Container>
+  </>
+)
 export default App;
